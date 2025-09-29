d4e74e7b3769df1e5c3ab6ff9bfe2dc7b441b4c9
Дарья Щербакова <daraaloec29@gmail.com>
2025-09-29 14:25:45 +0300

    creating a structure for vacancy_service.py

--- src/api_client.py
+++ src/api_client.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
+import requests
 
 
-class BaseApiClient(ABC):
+class Parser(ABC):
     @abstractmethod
     def connecting_to_the_api(self):
         """Метод подключения к API."""
@@ -13,13 +14,30 @@ class BaseApiClient(ABC):
         pass
 
 
-class HHApiClient(BaseApiClient):
+class HH(Parser):
+
+    def __init__(self, file_worker):
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
+        super().__init__(file_worker)
+
+    def load_vacancies(self, keyword):
+        self.params['text'] = keyword
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+
     def connecting_to_the_api(self):
         """Метод подключения к API."""
         pass
     # реализовать вызов в __receiving_vacancies_separately перед отправкой запроса
     # реализовать отправку запроса на базовый URL
     # реализовать проверку статус-кода ответа
+    # Ссылка на API: https://github.com/hhru/api/.
 
     def __receiving_vacancies_separately(self):
         """Приватный метод получения вакансий отдельно"""
