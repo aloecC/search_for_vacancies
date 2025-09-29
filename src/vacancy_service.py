e093d9e6c98d5b5f935423f10f376e3e3e2378bc
Дарья Щербакова <daraaloec29@gmail.com>
2025-09-29 13:03:42 +0300

    creating a structure for vacancy_service.py

---
+++ src/vacancy_service.py
@@ -0,0 +1,30 @@
+
+
+class BaseVacancyService:
+    #Реализовать __slots__ для экономии данных
+    pass
+
+    def __init__(self):
+        #Минимум 4 аргумента
+        pass
+
+    def compare_hour_ly_wage(self):
+        """метод сравнения вакансий с повременной оплатой."""
+        pass
+
+    def compare_piece_rate(self):
+        """метод сравнения вакансий с сдельной оплатой."""
+        pass
+
+    def compare_wage_types(self):
+        """метод сравнения вакансий с разными типами оплаты."""
+        pass
+
+    def data_validation_time_based_payment(self):
+        """метод валидации данных (повременный расчет)"""
+        pass
+
+    def data_validation_urgent_payment(self):
+        """метод валидации данных (сдельный расчет)"""
+        pass
+
